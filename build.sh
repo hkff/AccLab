01f0fd0940378530efaad468c4c8d80d21952550
Benghabrit Walid <infigmo@gmail.com>
2015-06-07 18:17:10 +0200

    Adding tspass source

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-tar -zcf pyAAL.tar --exclude='*.idea' --exclude='*.pyc' --exclude='coverage/html/*' --exclude='simul/*' --exclude='*.toc' --exclude='*.blg' --exclude='*.bbl' --exclude='*.aux' --exclude='*.out' --exclude='*.log' pyAAL/  
+tar -zcf pyAAL.tar --exclude='*.idea' --exclude='*.pyc' --exclude='coverage/html/*' --exclude='simul/*' --exclude='*.toc' --exclude='*.blg' --exclude='*.bbl' --exclude='*.aux' --exclude='*.out' --exclude='*.log'  --exclude='examples/test1.aal' --exclude='examples/simul.aal' pyAAL/  
