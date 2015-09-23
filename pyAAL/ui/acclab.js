333b89f7c14ef12863bb3b4f3277e98d72af767d
hkff <infigmo@gmail.com>
2015-09-23 11:12:21 +0200

    acclab.js rename

--- pyAAL/ui/acclab.js
+++ pyAAL/ui/acclab.js
@@ -1,6 +1,6 @@
 //////////////////////////////////////////////////////////
 //
-//  AccLab UI BETA V 1.0 : diagramEditor.js
+//  AccLab UI BETA V 1.0 : acclab.js
 //
 // Copyright (C) 2014 Walid Benghabrit
 //
