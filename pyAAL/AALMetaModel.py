001bf47def4cd2b5ae95b9a44619da5792f1b60c
hkff <infigmo@gmail.com>
2017-05-16 21:17:11 +0200

    Purpose Fix

--- pyAAL/AALMetaModel.py
+++ pyAAL/AALMetaModel.py
@@ -1510,6 +1510,9 @@ class m_action(m_aexp):
     def to_ltl(self, auth=""):
         args = []
         res = ""
+        # HANDLE purpose
+        if len(self.purpose) > 0:
+            res += "("
         # HANDLE time
         if self.time is not None:
             if self.time.action == m_booleanOp.O_before:
@@ -1534,6 +1537,9 @@ class m_action(m_aexp):
         res += ")"
         if self.time is not None:
             res += "))"
+
+        if len(self.purpose) > 0:
+            res += " => " + str(self.purpose[0]) + ")"
         return res
 
     def to_natural(self, kw=True, auth=False):
